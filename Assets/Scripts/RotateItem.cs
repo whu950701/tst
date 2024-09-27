baf3145b97ce068bffd7c7ce60be2abd79837fe1
123 <whu950701@gmail.com>
2024-09-27 20:23:09 +0800

    129

--- Assets/Scripts/RotateItem.cs
+++ Assets/Scripts/RotateItem.cs
@@ -4,28 +4,25 @@ public class RotateItem : MonoBehaviour
 {
     public float rotationSpeed = 200.0f;  // 旋转速度
 
-    private GameObject currentItem;
-
-    void Start()
+    private void Start()
     {
         enabled = false; // 初始状态下禁用旋转功能
     }
 
-    void Update()
+    private void Update()
     {
-        // 按住鼠标中键旋转物品
-        if (Input.GetMouseButton(2))
-        {
-            RotateObject();
-        }
+        // 根据鼠标移动来旋转物品
+        RotateObject();
     }
 
-    void RotateObject()
+    private void RotateObject()
     {
+        // 获取鼠标移动的水平和垂直位移
         float horizontal = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
         float vertical = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
 
-        transform.Rotate(Vector3.up, -horizontal, Space.World);
-        transform.Rotate(Vector3.right, vertical, Space.World);
+        // 根据鼠标移动旋转物品
+        transform.Rotate(Vector3.up, -horizontal, Space.World);  // 水平旋转
+        transform.Rotate(Vector3.right, vertical, Space.World);  // 垂直旋转
     }
 }
